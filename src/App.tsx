7ca671bc2f0c3d14b2d844b199a185e032f85af8
Luis <pmi_lfritzen@uolinc.com>
2024-10-03 22:45:11 -0300

    Init Pico LP

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { ThemeProvider } from '@/components/shells/theme-provider';
+// import Router from './pages/router';
+
+const App = () => {
+  return (
+    // <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    //   <Router />
+    // </ThemeProvider>
+    <div className="h-full w-full bg-primary">
+      <div></div>
+    </div>
+  );
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgr({
+      svgrOptions: {
+        exportType: 'default',
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: '**/*.svg',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
