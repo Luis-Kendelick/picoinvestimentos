9b1893bb13a03ca1224adb8c2cdb8efdf736a766
Luis <pmi_lfritzen@uolinc.com>
2024-10-11 05:53:24 -0300

    add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { Toaster } from './components/ui/toaster.tsx';
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <Toaster />
+    <Analytics />
   </StrictMode>,
 );
