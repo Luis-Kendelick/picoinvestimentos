14ae52b0452d9b2ce0ae29787fcd5a7a6a98aba9
Luis Fernando <luisfafritzen@gmail.com>
2025-07-16 18:53:22 -0300

    add: include carta mensal document for July 2025 and update document structure

--- src/utils/docs.ts
+++ src/utils/docs.ts
@@ -1,56 +1,69 @@
-import { compareAsc } from 'date-fns'
+import { compareAsc } from 'date-fns';
 
 interface IDoc {
-    file: string
-    date: string
+  file: string;
+  date: string;
 }
 
 export const docs = {
-    monthlyReports: [{
-        file: 'carta-mensal-12-24.pdf',
-        date: '2024-12-02',
-    }, {
-        file: 'carta-mensal-01-25.pdf',
-        date: '2025-01-02',
-    }, {
-        file: 'carta-mensal-02-25.pdf',
-        date: '2025-02-02',
-    }, {
-        file: 'carta-mensal-03-25.pdf',
-        date: '2025-03-02',
-    }, {
-        file: 'carta-mensal-04-25.pdf',
-        date: '2025-04-02',
-    }, {
-        file: 'carta-mensal-05-25.pdf',
-        date: '2025-05-02',
-    }, {
-        file: 'carta-mensal-06-25.pdf',
-        date: '2025-06-02',
-    }],
-    newsletters: [
-        {
-            file: 'newsletter-12-24.pdf',
-            date: '2024-12-02',
-        },
-        {
-            file: 'newsletter-01-25.pdf',
-            date: '2025-01-02',
-        },
-    ],
-}
+  monthlyReports: [
+    {
+      file: 'carta-mensal-12-24.pdf',
+      date: '2024-12-02',
+    },
+    {
+      file: 'carta-mensal-01-25.pdf',
+      date: '2025-01-02',
+    },
+    {
+      file: 'carta-mensal-02-25.pdf',
+      date: '2025-02-02',
+    },
+    {
+      file: 'carta-mensal-03-25.pdf',
+      date: '2025-03-02',
+    },
+    {
+      file: 'carta-mensal-04-25.pdf',
+      date: '2025-04-02',
+    },
+    {
+      file: 'carta-mensal-05-25.pdf',
+      date: '2025-05-02',
+    },
+    {
+      file: 'carta-mensal-06-25.pdf',
+      date: '2025-06-02',
+    },
+    {
+      file: 'carta-mensal-07-25.pdf',
+      date: '2025-07-02',
+    },
+  ],
+  newsletters: [
+    {
+      file: 'newsletter-12-24.pdf',
+      date: '2024-12-02',
+    },
+    {
+      file: 'newsletter-01-25.pdf',
+      date: '2025-01-02',
+    },
+  ],
+};
 
 export const dateOrderedDocs = (): {
-    monthlyReports: IDoc[], newsletters: IDoc[]
+  monthlyReports: IDoc[];
+  newsletters: IDoc[];
 } => {
-    const orderedMonthlyReportDocs = docs.monthlyReports.sort((a, b) => {
-        return compareAsc(a.date, b.date)
-    })
-    const orderedNewslettersDocs = docs.newsletters.sort((a, b) => {
-        return compareAsc(a.date, b.date)
-    })
-    return {
-        monthlyReports: orderedMonthlyReportDocs,
-        newsletters: orderedNewslettersDocs
-    }
-}
+  const orderedMonthlyReportDocs = docs.monthlyReports.sort((a, b) => {
+    return compareAsc(a.date, b.date);
+  });
+  const orderedNewslettersDocs = docs.newsletters.sort((a, b) => {
+    return compareAsc(a.date, b.date);
+  });
+  return {
+    monthlyReports: orderedMonthlyReportDocs,
+    newsletters: orderedNewslettersDocs,
+  };
+};
